670cdd2e6736a38b494053a1dd3bff166964dc72
Bansal <amitb@MacBook-Pro.local>
2020-08-28 10:56:30 +0530

    Initial commit

---
+++ app/src/main/java/com/skipper/firstapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.skipper.firstapp;
+import android.app.Application;
+import android.util.Log;
+import com.appsflyer.AppsFlyerLib;
+import com.appsflyer.AppsFlyerConversionListener;
+import java.util.Map;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
